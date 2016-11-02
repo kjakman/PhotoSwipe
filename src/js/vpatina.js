5549fbd1706fe70bd0b7f6d179c05384aa522bd1
Kjetil Larsen <github@kjakman.com>
2016-11-02 12:10:54 +0100

    test 12345

--- src/js/vpatina.js
+++ src/js/vpatina.js
@@ -140,7 +140,7 @@ var openPhotoSwipe = function(cid, index, galleryElement, disableAnimation, from
         // isFake    - true when content is added to fake caption container
         //             (used to get size of next or previous caption)
     
-        // test 123
+        // test 12345
         /**
         if(!item.title) {
             captionEl.children[0].innerHTML = '';
